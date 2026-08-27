b43005f04b73970d92044e8f11f9caff1d87efc1
Aaxtroence <133886539+Aaxtroence@users.noreply.github.com>
2026-08-27 17:16:28 +0300

    feat: add custom application exceptions

---
+++ ConferenceBookingApi/Exceptions/ConflictException.cs
@@ -0,0 +1,6 @@
+﻿namespace ConferenceBookingApi.Exceptions;
+
+public class ConflictException : Exception
+{
+    public ConflictException(string message) : base(message) { }
+}
---
+++ ConferenceBookingApi/Exceptions/ValidationException.cs
@@ -0,0 +1,6 @@
+﻿namespace ConferenceBookingApi.Exceptions;
+
+public class ValidationException : Exception
+{
+    public ValidationException(string message) : base(message) { }
+}
