82974de766852978f0a1984f5ada36c265c1e966
Aaxtroence <133886539+Aaxtroence@users.noreply.github.com>
2026-08-30 15:58:14 +0300

    feat: add booking request and result DTOs

---
+++ ConferenceBookingApi/DTOs/BookingResultDto.cs
@@ -0,0 +1,13 @@
+﻿namespace ConferenceBookingApi.DTOs;
+
+public class BookingResultDto
+{
+    public int Id { get; set; }
+    public int RoomId { get; set; }
+    public DateTime StartTime { get; set; }
+    public DateTime EndTime { get; set; }
+    public decimal RoomCost { get; set; }
+    public decimal EquipmentCost { get; set; }
+    public decimal TotalPrice { get; set; }
+    public List<string> SelectedEquipmentNames { get; set; } = new();
+}
---
+++ ConferenceBookingApi/DTOs/CreateBookingDto.cs
@@ -0,0 +1,9 @@
+﻿namespace ConferenceBookingApi.DTOs;
+
+public class CreateBookingDto
+{
+    public int RoomId { get; set; }
+    public DateTime StartTime { get; set; }
+    public DateTime EndTime { get; set; }
+    public List<int> SelectedEquipmentIds { get; set; } = new();
+}
